aae65c4b594c82d5b9fb5617c74c003ccd817ae4
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-08-02 21:58:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// Importa a biblioteca React.
+import React from "react";
+
+// Importa o CSS.
+import "./App.css";
+
+// Criado um componente chamado App.
+function App() {
+  return <div>App React</div>;
+}
+
+export default App; // Exporta o componente App.
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+// Está importando a biblioteca React e ReactDOM.
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Está importando o componente.
+import App from "./App.jsx";
+
+// Está renderizando o componente App no elemento com o ID root do HTML.
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
